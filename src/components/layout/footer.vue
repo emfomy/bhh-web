7189deae50d4fdc42f836d947a7d3d16033c0fbc
Mu Yang <emfomy@gmail.com>
2019-06-07 23:16:55 +0800

    Update footer.

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -2,7 +2,7 @@
   <footer class="shadow-sm border-top bg-white w-100 py-2">
     <b-container class="text-center">
       <small class="text-muted">
-        Design by <b-link href="https://muyang.pro" target="_blank">Mu Yang</b-link>.
+        Host by <b-link href="https://muyang.pro" target="_blank">Mu Yang</b-link>.
       </small>
     </b-container>
   </footer>
